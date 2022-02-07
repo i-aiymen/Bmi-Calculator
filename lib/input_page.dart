10ec248e93625d251eb1cf742133db29da073012
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2022-02-07 22:18:31 +0530

    Created the basic skeleton on BMI App

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,12 +9,42 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33),)),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+              ],
+            )),
+            Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+            Expanded(
+                child: Row(
+              children: [
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+                Expanded(child: ReusableCard(colour: Color(0xFF1D1E33))),
+              ],
+            )),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -25,21 +27,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-    );
-  }
-}
